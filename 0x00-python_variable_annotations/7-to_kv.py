c32157bdb545aba4c0c768757f829bb027d719af
KUM JUDE <kumjude09@gmail.com>
2023-07-06 22:55:14 +0100

    a function that takes a string k and an int OR float v as arguments and returns a tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""return a turple"""
+""" Takes str and int or float, returns tuple """
 
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """takes a string k and an int OR float v as arguments and returns a tuple"""
+    """ Returns tuple """
     new_tup: Tuple[str, Union[int, float]] = (k, v**2)
     return new_tup
