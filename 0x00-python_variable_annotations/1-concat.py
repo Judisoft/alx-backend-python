9b7cb93bcce2c76b6f5377fd8a715467b7d14689
KUM JUDE <kumjude09@gmail.com>
2023-07-06 20:46:22 +0100

    annotated function that concatenates two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Returns a concatenated string"""
+
+
+def concat(str1: str, str2: str)->str:
+    """function concatenates strings and returns strings"""
+    return str1 + str2
